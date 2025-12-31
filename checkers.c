102258a21bacbab77b55d86fd2d6910ed6cf6e8d
Aviuss <konradbb09@gmail.com>
2025-12-31 20:00:51 +0100

    init game and better prinitng

--- checkers.c
+++ checkers.c
@@ -6,19 +6,35 @@ void initGame(struct Game* game) {
     game->turn = 'w';
     for (int y = 0; y < 8; y++) {
         for (int x = 0; x < 8; x++) {
-            game->board[y][x] = 'x';
+            if (x % 2 != y % 2) {
+                game->board[y][x] = 'x';
+                continue;
+            }
+
+            if (y < 3) {
+                game->board[y][x] = 'w';
+            } else if (y > 4) {
+                game->board[y][x] = 'b';
+            } else {
+                game->board[y][x] = ' ';
+            }
+
         }
     }
 }
 
 void printBoard(struct Game* game) {
     printf("  A B C D E F G H\n");
-    for(int i = 0; i < 8; i++) {
-        printf("%d ", 8-i);
-        for(int j = 0; j < 8; j++) {
-            printf("%c ", game->board[i][j]);
+    for(int y = 7; y >= 0; y--) {
+        printf("%d ", y);
+        for(int x = 0; x < 8; x++) {
+            if (game->board[y][x] == 'x') {
+                printf("  ");
+            } else {
+                printf("%c ", game->board[y][x]);
+            }
         }
-        printf("%d", 8-i);
+        printf("%d", y);
         printf("\n");
     }
     printf("  A B C D E F G H\n");
